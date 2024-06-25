b9768616ca09a5ab167546c424f1e5630fb30153
osoken <osoken@osoekawalab.com>
2024-06-26 00:28:52 +0900

    fix: add readme and change build tool

--- src/pyvlq/__init__.py
+++ src/pyvlq/__init__.py
@@ -1,5 +1,5 @@
 from .core import decode, decode_stream, encode
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
-__all__ = ["decode", "encode", "decode_stream"]
+__all__ = ["__version__", "decode", "encode", "decode_stream"]
