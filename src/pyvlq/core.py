19495f54f3ea65f8acd22f1c5c301f9783d32601
osoken <osoken@osoekawalab.com>
2024-06-25 22:40:18 +0900

    feat: add decode_stream

--- src/pyvlq/core.py
+++ src/pyvlq/core.py
@@ -1,5 +1,10 @@
+from io import BytesIO
+
+
 def encode(value: int) -> bytes:
     """Encode a value into a VLQ byte sequence.
+
+    Encode a value into a VLQ byte sequence.
 
     Args:
         value: The value to encode.
@@ -40,6 +45,8 @@ def encode(value: int) -> bytes:
 def decode(data: bytes) -> int:
     """Decode a VLQ byte sequence into a value.
 
+    Decode a VLQ byte sequence into a value.
+
     Args:
         data: The VLQ byte sequence.
 
@@ -69,3 +76,48 @@ def decode(data: bytes) -> int:
         if not byte & 0x80:
             return value
     raise ValueError("Malformed VLQ byte sequence")
+
+
+def decode_stream(data: BytesIO) -> int:
+    """Decode a VLQ byte sequence into a value.
+
+    Decode a VLQ byte sequence from a stream.
+    This function reads from a stream. It consumes the stream up to the end of the VLQ byte sequence.
+    This function is useful when the byte sequence is part of a larger stream.
+
+    Args:
+        data: The VLQ byte sequence stream.
+
+    Returns:
+        The decoded value.
+
+    Raises:
+        ValueError: If the byte sequence is malformed.
+
+    >>> decode_stream(BytesIO(b'\\x00'))
+    0
+    >>> decode_stream(BytesIO(b'\\x01'))
+    1
+    >>> decode_stream(BytesIO(b'\\x7f'))
+    127
+    >>> decode_stream(BytesIO(b'\\x81\\x80\\x80\\x80\\x00'))
+    268435456
+    >>> buf = BytesIO(b'\\x81\\x80\\x00\\x81\\x80')
+    >>> decode_stream(buf)
+    16384
+    >>> buf.read()
+    b'\\x81\\x80'
+    >>> decode_stream(BytesIO(b'\\x81\\x80'))
+    Traceback (most recent call last):
+     ...
+    ValueError: Malformed VLQ byte sequence
+    """
+    value = 0
+    while True:
+        byte = data.read(1)
+        if not byte:
+            raise ValueError("Malformed VLQ byte sequence")
+        value <<= 7
+        value |= byte[0] & 0x7F
+        if not byte[0] & 0x80:
+            return value
